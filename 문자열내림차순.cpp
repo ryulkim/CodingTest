10c5b5ffd56ace6f06b5e5383075a0681cfc9424
ryulkim <58160874+ryulkim@users.noreply.github.com>
2021-02-14 01:44:52 +0900

    Create 문자열내림차순.cpp
    프로그래머스
    https://programmers.co.kr/learn/courses/30/lessons/12917,

---
+++ 문자열내림차순.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(int r1,int r2){
+    return r1>r2;
+}
+
+string solution(string s) {
+    string answer = "";
+    int size=s.length();
+    vector<int> v;
+    for(int i=0;i<size;i++){
+        v.push_back(int(s.at(i)));
+    }
+    sort(v.begin(),v.end(),cmp);
+    for(int i=0;i<size;i++){
+        answer+=char(v.at(i));
+    }
+    return answer;
+}
