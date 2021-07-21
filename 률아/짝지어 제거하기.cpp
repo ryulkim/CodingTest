500fadfcc78b64e27283d7136a301f313cac37e3
ryulkim <58160874+ryulkim@users.noreply.github.com>
2021-07-21 23:42:21 +0900

    짝지어 제거하기
    https://programmers.co.kr/learn/courses/30/lessons/12973

---
+++ 률아/짝지어 제거하기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    int answer=-1;
+    stack<int> str;
+    int len=s.length();
+    for(int i=0;i<len;i++){
+        if(str.empty()||str.top()!=s[i]){
+            str.push(s[i]);
+        }
+        else{
+            str.pop();
+        }
+    }
+    
+    if(str.empty()){
+        answer=1;
+    }
+    else{
+        answer=0;
+    }
+
+    return answer;
+}
