54ef5d46aff3d0cdd7664ba647f6241e6ddb8865
profrog <ache159@naver.com>
2021-09-03 13:06:50 +0900

    Create week1_2.cpp

---
+++ 민규/문해기/week1_2.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+
+vector<int> block;
+int min0 = 1001;
+int max0 = 0;
+string solution(string &data)
+{
+    try {
+        
+        
+        if (data == "return") {
+            min0 = 0;
+            max0 = 0;
+            block.resize(0);
+        }
+
+        else
+        {
+            if (data[0] == 'B')
+            {
+                block.pop_back();
+                min0 = *min_element(block.begin(), block.end());
+                max0 = *max_element(block.begin(), block.end());
+                return "blank";
+            }
+
+            else{
+                int value_a = stoi(data);
+        
+                if (block.empty())
+                {
+                    min0 = value_a;
+                    max0 = value_a;
+                }
+
+                else {
+                    min0 = (min0 > value_a) ? value_a : min0;
+                    max0 = (max0 < value_a) ? value_a : max0;
+                }
+
+                block.push_back(value_a);
+            }
+        }
+
+        if (block.empty())
+            return "blank";
+
+        //cout << block.size() << endl;
+        return to_string(min0) + " " + to_string(max0);
+    }
+
+    catch (exception& e)
+    {
+        cout << e.what() << endl;
+        //cout << data << endl;
+    }
+}
+
+
+int main(void)
+{
+    int m,n;
+    string data;
+
+    cin >> m;
+
+    for (int x = 0; x < m; x++)
+    {
+        cin  >> n;
+        string data = "return";
+        solution(data);
+        for (int y = 0; y < n; y++)
+        {
+            cin >> data;
+            
+            if (data == "1")
+                cin >> data;
+
+            else
+                data = "B";
+
+            //cout << data << endl;
+            string final = solution(data);
+
+            if (final != "blank")
+            {
+                cout << final;
+                cout << "\n";
+            }
+        }
+    }
+    return 0;
+}
