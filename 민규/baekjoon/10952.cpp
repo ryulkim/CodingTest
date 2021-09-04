238edc1d2bc352da2d0998f4a124eb4553c8b43f
profrog <ache159@naver.com>
2021-09-04 18:01:05 +0900

    Create 10952.cpp

---
+++ 민규/baekjoon/10952.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(void)
+{
+ while(true)
+ {
+  int a,b;
+
+  cin >> a >> b;
+
+  if(a== 0 && b==0)
+   break;
+
+  else
+   cout << a + b << endl;
+
+ }
+
+ return 0;
+}
