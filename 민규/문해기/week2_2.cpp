8639ec0a5ee84a691c855751246f38fe01f1203b
profrog <ache159@naver.com>
2021-09-11 17:36:12 +0900

    Update week2_2.cpp

--- 민규/문해기/week2_2.cpp
+++ 민규/문해기/week2_2.cpp
@@ -6,29 +6,31 @@ using namespace std;
 
 vector<pair<int,int>> resource;
 int m;
-
+int inha_resource = 0;
+int bi_resource = 0;
 
 bool checking(pair<int,int>i , pair<int,int>j)
 {
- return i.first > j.first;
+ return i.first - j.second < j.first -i.second;
 }
 
-
 int solution()
-{
- int inha_resource = 0;
- int bi_resource = 0;
+{ 
+  inha_resource = 0;
+  bi_resource = 0;
 
- for(int x = resource.size()-1 ; x > resource.size()-1 - (int)m/2; x--)
-   bi_resource += resource[x].second;
+ while(!resource.empty())
+  {
+   inha_resource += resource.back().first;
+   resource.pop_back();
 
- for(int x = 0 ; x < m - (int)m/2; x++)
-   inha_resource += (resource[x].first+ resource[x].second);
+   if(resource.empty()) break;
+   bi_resource += resource.back().second;
+   resource.pop_back();
+  }
  
  return inha_resource - bi_resource;
 }
-
-
 
 int main(void)
 {
@@ -39,15 +41,12 @@ int main(void)
  int t; cin >> t;
  for(int x = 0 ; x < t ; x++) //CASE
  {
-  resource.resize(0);
-
   cin >> m;
   for(int y = 0 ; y < m ;y++) //RES_INPUT
   {
    int i1,b1; cin >> i1 >> b1;
-   resource.push_back(pair<int,int>(i1-b1,b1));
-  }
-   
+   resource.push_back(pair<int,int>(i1,b1));
+  }  
   sort(resource.begin(),resource.end(),checking);
   cout << solution() << "\n";
  }
