af6ed1f96243edf0f9fb5bad39beb1375372c002
profrog <ache159@naver.com>
2021-09-17 01:36:03 +0900

    Create 10950.cpp

---
+++ 민규/baekjoon/10950.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+ int m; cin >> m;
+
+ for(int x = 0 ; x < m ; x++)
+ {
+  int a,b; cin >> a >> b;
+  cout << a+b << "\n";
+ }
+
+ return 0;
+}
