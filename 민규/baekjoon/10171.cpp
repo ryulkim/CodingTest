fee3839359e1c3d53b69ca38be5c718ac09d9f68
profrog <ache159@naver.com>
2021-09-17 01:23:42 +0900

    Create 10171.cpp

---
+++ 민규/baekjoon/10171.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(void)
+{
+ cout << "\\    /\\" << "\n";
+ cout << " )  ( ')" << "\n";
+ cout << "(  /  )" << "\n";
+ cout << " \\(__)|" << "\n";
+
+ return 0;
+}
