dadd9f0e4e416aaa1da245c05280a4e6c6d443b6
profrog <ache159@naver.com>
2021-09-17 01:37:51 +0900

    Create 11021.cpp

---
+++ 민규/baekjoon/11021.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+ int m; cin >> m;
+
+ for(int x = 1 ; x <= m ; x++)
+ {
+  int a,b; cin >> a >> b;
+  cout <<  "Case #" << x << ": " << a+b << "\n";
+ }
+
+ return 0;
+}
