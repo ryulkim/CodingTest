319105c568ae47b87ea364233931cf1bf5e67cb4
unknown <hobby519@naver.com>
2021-09-22 00:24:19 +0900

    excercise1' '

--- 률아/오픈소스/loop/exercise1.py
+++ 률아/오픈소스/loop/exercise1.py
@@ -1,7 +1,7 @@
 coin=0;
-print("1: Insert coin | 2: Item list | 3: Select fruit | 4: Change money | Q: exit");
 
 while 1:
+    print("1: Insert coin | 2: Item list | 3: Select fruit | 4: Change money | Q: exit");
     num=input("Please select menu: ");
     if num=="1":
         amount=input("How much money do you want to put in? ");
@@ -11,13 +11,18 @@ while 1:
         print('::::1. Orange: 300 :::: :::: 2. Shine muscat: 1000::::');
     elif num=="3":
         select=input("Which fruit do you want?");
-        if select=="1":
+        if select=="1" and coin>=300:
             print("You get an orange");
             coin-=300;
-        elif select=="2":
+            print("You have %d coins"%coin);
+        elif select=="1" and coin<300:
+            print("You are short of money\nReturn to start menu");
+        elif select=="2" and coin>=1000:
             print("You get an shine muscat");
             coin-=1000;
-        print("You have %d coins"%coin);
+            print("You have %d coins"%coin);
+        elif select=="2" and coin<1000:
+            print("You are short of money\nReturn to start menu");
     elif num=="4":
         print("You get %d coins back"% coin);
         coin=0;
