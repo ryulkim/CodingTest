d1bd0e4787f32e1b04345da41bbc61c8e62b70d3
unknown <hobby519@naver.com>
2021-09-22 00:48:19 +0900

    excercise1

---
+++ 률아/오픈소스/DataType/exercise1.py
@@ -0,0 +1,17 @@
+#나만의 답
+"""
+A, A_phone=input().split(' ')
+B, B_phone=input().split(' ')
+
+print(A.capitalize(), A_phone.replace('-',''))
+print(B.capitalize(), B_phone.replace('-',''))
+
+"""
+
+a="abel"
+b="nenny"
+numberA="010-1234-5678"
+numberB="010-9876-5432"
+
+print(a,numberA,"-->",a.capitalize(),numberA.replace('-',''))
+print(b,numberB,"-->",b.capitalize(),numberB.replace('-',''))
