9c4c9e36ca53608cc895f59e441ef759db707d74
profrog <ache159@naver.com>
2021-10-21 16:08:00 +0900

    Update week1_2.cpp

--- 민규/문해기/week1_2.cpp
+++ 민규/문해기/week1_2.cpp
@@ -1,103 +1,57 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <sstream>
-#include <algorithm>
 using namespace std;
 
 
-vector<int> block;
-int min0 = 1001;
-int max0 = 0;
-string solution(string &data)
+int min_max(vector<int>& data_0)
 {
-    try {
-        
-        
-        if (data == "return") {
-            min0 = 0;
-            max0 = 0;
-            block.resize(0);
-        }
+ int last_index = data_0.size()-1;
 
-        else
-        {
-            if (data[0] == 'B')
-            {
-             if(!block.empty()){
-                block.pop_back();
-                min0 = *min_element(block.begin(), block.end());
-                max0 = *max_element(block.begin(), block.end());
-             }
-                return "blank";
-            }
+ for(int x = 1 ; x <= last_index ; x++)
+   if(data_0[x])
+   {cout << x << " "; break;}
 
-            else{
-                int value_a = stoi(data);
-        
-                if (block.empty())
-                {
-                    min0 = value_a;
-                    max0 = value_a;
-                }
-
-                else {
-                    min0 = (min0 > value_a) ? value_a : min0;
-                    max0 = (max0 < value_a) ? value_a : max0;
-                }
-
-                block.push_back(value_a);
-            }
-        }
-
-        if (block.empty())
-            return "blank";
-
-        //cout << block.size() << endl;
-        return to_string(min0) + " " + to_string(max0);
-    }
-
-    catch (exception& e)
-    {
-        cout << e.what() << endl;
-        //cout << data << endl;
-    }
-
-    return "";
+ for(int x = last_index; x > 0; x--)
+   if(data_0[x])
+    {cout << x << "\n"; break;}
+ 
+ return 0;
 }
 
 
+int solution(int case_1)
+{
+ vector<int> data_0(1001,0);
+ vector<int> list_0(0,0);
+
+ for(int x = 0 ; x < case_1 ; x++)
+ {
+  int n; cin >> n;
+  if(n == 1)
+  {
+   int data_00; cin >> data_00;
+   list_0.push_back(data_00); data_0[data_00]++;
+   min_max(data_0);
+  }
+
+  else if(list_0.size())
+  {
+   data_0[list_0.back()]--; list_0.pop_back();
+  }
+ }
+ return 0;
+}
+
 int main(void)
 {
-    int m,n;
-    string data;
+ ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
+ int all_case; cin >> all_case;
 
-    cin >> m;
+ for(int x = 0; x < all_case; x++)
+ {
+  int case_1; cin >> case_1;
+  solution(case_1);
+ }
 
-    for (int x = 0; x < m; x++)
-    {
-        cin  >> n;
-        string data = "return";
-        solution(data);
-        for (int y = 0; y < n; y++)
-        {
-            cin >> data;
-            
-            if (data == "1")
-                cin >> data;
-
-            else
-                data = "B";
-
-            //cout << data << endl;
-            string final = solution(data);
-
-            if (final != "blank")
-            {
-                cout << final;
-                cout << "\n";
-            }
-        }
-    }
-    return 0;
+ return 0;
 }
