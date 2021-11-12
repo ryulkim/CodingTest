926cae7adbcc3a1e4cd9cc714b8411a8e47c9715
profrog <ache159@naver.com>
2021-11-13 01:46:10 +0900

    Create week11_1.cpp

---
+++ 민규/문해기/week11_1.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+const int index_a = 97;
+const int count_a = 26;
+const string de = "@@";
+vector<int> alpa_num(count_a, 0);
+vector<int> alpa_copy;
+vector<pair<string,int>> menu;
+int max_count0 = 0;
+
+
+
+void healing(vector<pair<int, int>>& call0)
+{
+	for (int x = 0; x < call0.size(); x++)
+	{
+		alpa_num[call0[x].first] += call0[x].second;
+	}
+}
+
+
+vector<pair<int, int>> dfs(int m_0, int count0)
+{
+	int count = 0; char inde_0 = menu[m_0].first.front();
+	vector<pair<int, int>> call0;
+
+	for (int x = 0; x < menu[m_0].first.length(); x++)
+	{
+		if (inde_0 == menu[m_0].first[x])
+			count++;
+
+		else
+		{
+		  if (alpa_num[int(inde_0) - index_a] - count < 0)
+		  {
+		    max_count0 = max(max_count0, count0); 
+		    return call0;
+		  }
+
+		  alpa_num[int(inde_0) - index_a] -= count;
+		  call0.push_back(pair<int, int>(int(inde_0) - index_a, count));
+		  inde_0 = menu[m_0].first[x]; count = 1;
+		}
+	}
+ 
+	string s = menu[m_0].first; menu[m_0].first = de;
+	for (int x = m_0+1; x < menu.size(); x++)
+	{
+		if (menu[x].first != de) {
+			vector<pair<int, int>> a = dfs(x, count0 + menu[m_0].second);
+			healing(a);		
+		}
+	}
+    
+	menu[m_0].first = s;
+	return call0;
+}
+
+
+
+int solution()
+{
+	for (int x0 = 0; x0 < menu.size(); x0++)
+	{
+	  vector<pair<int, int>> a = dfs(x0, 0);
+	  healing(a);
+	}
+
+	return max_count0;
+}
+
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+
+	int num0;
+	for (int x = 0; x < count_a; x++)
+	{
+		cin >> num0;
+		alpa_num[x] = num0;
+	}
+
+	cin >> num0;
+
+	for (int x = 0; x < num0; x++)
+	{
+		string s; int n; cin >> s >> n;
+		sort(s.begin(), s.end());
+		menu.push_back(pair<string, int>(s, n));
+	}
+
+	cout << solution() << "\n";
+	return 0;
+}
