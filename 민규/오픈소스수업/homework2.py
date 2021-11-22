8bba91e276378443f980e79369b69a043347e4e1
profrog <ache159@naver.com>
2021-11-23 02:02:10 +0900

    Update homework2.py

--- 민규/오픈소스수업/homework2.py
+++ 민규/오픈소스수업/homework2.py
@@ -7,7 +7,6 @@ d2l.DATA_HUB['ml-100k'] = (
     'http://files.grouplens.org/datasets/movielens/ml-100k.zip',
     'cd4dcac4241c8a4ad7badc7ca635da8a69dddb83')
 
-
 def read_data_ml100k():
     data_dir = d2l.download_extract('ml-100k')
     names = ['user_id', 'movie_id', 'rating', 'timestamp']
@@ -18,7 +17,7 @@ def read_data_ml100k():
     user = pd.read_csv(os.path.join(data_dir, 'u.user'), '|', names=names2,
                        engine='python')
     
-    names3 = ['movie id', 'movie name' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
+    names3 = ['movie_id', 'movie_name' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
     item = pd.read_csv(os.path.join(data_dir, 'u.item'), '|',names=names3,
@@ -28,8 +27,11 @@ def read_data_ml100k():
 
 data, user,items = read_data_ml100k()
 
-print(items)
+data2 = data[data['rating'] > 4]
 
-#frame = pd.DataFrame(item, columns = ['item_id', 'name'])
-#frame
+quiz1 = pd.merge(data2, item)
+quiz1 = quiz1.drop_duplicates(['movie_id'], keep = 'first')
+print("highest number of ratings :  for all user\n")
+print(quiz1['movie_name'].head(20))
 
+############quiz 1
