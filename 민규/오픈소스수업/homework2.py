bdb76e89ae0bd4399358e6975903c49d0d405f53
profrog <ache159@naver.com>
2021-11-24 14:57:49 +0900

    Update homework2.py

--- 민규/오픈소스수업/homework2.py
+++ 민규/오픈소스수업/homework2.py
@@ -9,7 +9,7 @@ d2l.DATA_HUB['ml-100k'] = (
     'http://files.grouplens.org/datasets/movielens/ml-100k.zip',
     'cd4dcac4241c8a4ad7badc7ca635da8a69dddb83')
 
-def read_data_ml100k():
+def movielens_1():
     data_dir = d2l.download_extract('ml-100k')
     names = ['user_id', 'movie_id', 'rating', 'timestamp']
     data = pd.read_csv(os.path.join(data_dir, 'u.data'), '\t', names=names,
@@ -25,15 +25,14 @@ def read_data_ml100k():
     item = pd.read_csv(os.path.join(data_dir, 'u.item'), '|',names=names3,
         encoding = "ISO-8859-1", engine='python')
     
-    return data,user,item
+    quiz0 = pd.merge(data, item)
+    quiz0 = pd.merge(quiz0, user)
+     
+    return quiz0
 
 #############################################33
 
-data, user,item = read_data_ml100k()
-
-quiz0 = pd.merge(data, item)
-quiz0 = pd.merge(quiz0, user)
-
+quiz0 = movielens_1()
 female0 = quiz0[quiz0['sex'] == 'F']
 male0 = quiz0[quiz0['sex'] == 'M']
 
@@ -105,16 +104,68 @@ for name in job_list:
 
 ##########################3
 
+for name in genre_list:
+ quiz01 = quiz0[quiz0[name] == 1]
+ group1 = quiz01.groupby('movie_name')['rating'].agg(**{ 'count' : 'count' , 'mean' : 'mean' , 'many': 'max'}).reset_index()
+ all_user1 = group1.sort_values(['count'] , ascending = False)
+ print(name + " all user + genre + count \n")   
+ print(all_user1[['movie_name' , 'count']])
+ print("\n")
+
+ all_user1 = group1.sort_values(['mean'] , ascending = False)
+ print(name + " all user + genre + average \n")   
+ print(all_user1[['movie_name' , 'mean']])
+ print("\n")
 
 
+#################################
 
 
+for name in genre_list:
+ quiz01 = female0[female0[name] == 1]
+ group1 = quiz01.groupby('movie_name')['rating'].agg(**{ 'count' : 'count' , 'mean' : 'mean' , 'many': 'max'}).reset_index()
+ all_user1 = group1.sort_values(['count'] , ascending = False)
+ print(name + " female + genre + count \n")   
+ print(all_user1[['movie_name' , 'count']])
+ print("\n")
 
+ all_user1 = group1.sort_values(['mean'] , ascending = False)
+ print(name + " female + genre + average \n")   
+ print(all_user1[['movie_name' , 'mean']])
+ print("\n")
 
+    
+#######################################
 
+for name in genre_list:
+ quiz01 = male0[female0[name] == 1]
+ group1 = quiz01.groupby('movie_name')['rating'].agg(**{ 'count' : 'count' , 'mean' : 'mean' , 'many': 'max'}).reset_index()
+ all_user1 = group1.sort_values(['count'] , ascending = False)
+ print(name + " all user + count \n")   
+ print(all_user1[['movie_name' , 'count']])
+ print("\n")
 
+ all_user1 = group1.sort_values(['mean'] , ascending = False)
+ print(name + " all user + average \n")   
+ print(all_user1[['movie_name' , 'mean']])
+ print("\n")
 
+##################################
 
+for name1 in job_list:
+ quiz01 = quiz0[quiz0['job'] == name1]
+ 
+ for name in genre_list:
+     #quiz01 = quiz01[quiz01[name] == 1]
+     group1 = quiz01.groupby('movie_name')['rating'].agg(**{ 'count' : 'count' , 'mean' : 'mean' , 'many': 'max'}).reset_index()
+     all_user1 = group1.sort_values(['count'] , ascending = False)
+     print(name + " " + name1  + " count \n")   
+     print(all_user1[['movie_name' , 'count']])
+     print("\n")
 
+     all_user1 = group1.sort_values(['mean'] , ascending = False)
+     print(name + " " + name1  +" average \n")   
+     print(all_user1[['movie_name' , 'mean']])
+     print("\n")
 
 
