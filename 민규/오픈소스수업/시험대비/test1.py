f86317a0cbc6f8acfce726d428a8465f06533c57
profrog <ache159@naver.com>
2021-12-16 03:04:22 +0900

    Create test1.py

---
+++ 민규/오픈소스수업/시험대비/test1.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+#data = np.random.randn(4,2)
+#data.shape
+#data.dtype
+
+list = [[1,3,2],[2,4,5],[1,2,3]]
+#data = np.array(list)
+#data
+
+#data = np.zeros(10)
+data = np.arange(15)
+#data = np.empty(10)
+#data2 = np.array(list, dtype = np.float64)
+#data = np.array(list, dtype = np.int32)
+#data = data.astype(data2.dtype)
+#data = np.arange(5)
+#1 / data ** 2 > data
+
+#data = data.mean(1)
+#data.reshape((4,1))
+#data.shape
+
+#data2 = data[5:8]
+#data2[:] = 9999
+#data = np.array(list)
+#data
+#data[:2,1:]
+
+#name = np.array(["he", "ee" , "we"])
+#name == "ee"
+#data = np.random.randn(3,4)
+#data[~(name == "ee")]
+
+#data = np.empty((4,4))
+
+#for i in range(4):
+ #data[i] = i
+
+#data
+
+
+#data = np.arange(32).reshape(8,4)
+#data = data[[0,1,2]][:,[3,2,1,0]]
+#data.T
