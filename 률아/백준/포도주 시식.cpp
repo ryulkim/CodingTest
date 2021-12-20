1a5a7b0d4f1cab5be47c9abc5cf499f00ae0e7a9
ryulkim <58160874+ryulkim@users.noreply.github.com>
2021-12-21 00:25:52 +0900

    포도주 시식(Silver 1)
    https://www.acmicpc.net/problem/2156

--- 률아/백준/포도주 시식.cpp
+++ 률아/백준/포도주 시식.cpp
@@ -5,33 +5,24 @@ int arr[10002][2];//행: 포도주, 열: 연속 개수
 int glass[10002];
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     int n, x, M = 0, sec=0;
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         cin >> glass[i];
-    }
-    arr[0][0] = glass[0];
-    arr[1][0] = glass[1];
-    arr[1][1] = glass[0] + glass[1];
-
-    M = max(arr[0][0], arr[1][0]);
-    M = max(M, arr[1][1]);
+    
+    arr[0][0] = glass[0]; arr[1][0] = glass[1]; arr[1][1] = glass[0] + glass[1];
+    M = max(arr[0][0], arr[1][0]); M = max(M, arr[1][1]);
 
     for (int i = 2; i < n; i++) {
         if (sec < max(arr[i - 2][0], arr[i - 2][1])) {
             sec = max(arr[i - 2][0], arr[i - 2][1]);
         }
-        arr[i][0] = sec + glass[i];
-        arr[i][1] = arr[i - 1][0] + glass[i];
+        arr[i][0] = sec + glass[i]; arr[i][1] = arr[i - 1][0] + glass[i];
         
-        if (M < max(arr[i][0], arr[i][1])) {
+        if (M < max(arr[i][0], arr[i][1]))
             M = max(arr[i][0], arr[i][1]);
-        }
-        
     }
     cout << M << '\n';
 }
