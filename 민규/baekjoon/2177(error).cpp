37d48b7b8fe5874f81b7b0b78dda567b7f7ec23b
profrog <ache159@naver.com>
2022-01-06 22:26:07 +0900

    Update 2177(error).cpp

--- 민규/baekjoon/2177(error).cpp
+++ 민규/baekjoon/2177(error).cpp
@@ -1,31 +1,64 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <list>
 using namespace std;
 
-int n,m;
-vector<pair<int,int>> data0; long double data1 = 0;
+
+int answer = 0;
+int n, m;
+vector<int> money_n;
+list<pair<int,int>> money_v;
+list<int> money_g;
 
 int main(void)
 {
- ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
- cin >> n >> m; 
- int value,count,answer = 0;
+	cin >> n >> m; money_n.assign(m, 0);
 
- for(int x = 0; x < n ;x++)
- {
-  cin >> value >> count;
+	for (int x = 0; x < n; x++)
+	{
+		int c1, c2; cin >> c1 >> c2;
 
-  if(value >= m)
-   answer += count;
+		if (c1 >= m) answer += c2;
+		else {
+			money_v.push_back(pair<int,int>(c1,c2));
+		}
+	}
 
-  else{
-   answer += count/(m/value);
+	//sort(money_v.begin(), money_v.end());
+	money_v.sort();
+	int c1 = money_v.back().first; int c2 = money_v.back().second;
+	money_v.pop_back();
+	money_g.assign(m - c1, c2);
 
-   if(count - (count/(m/value) *(m/value)))
-    data1 += (long double)(count - (count/(m/value) *(m/value)))/(long double)(m/value);
-  } 
- }
- 
- cout << answer + (int)data1<< "\n";
- return 0;
+	while (money_v.size())
+	{
+		int c1 = money_v.back().first; int c2 = money_v.back().second;
+		money_v.pop_front();
+
+		while (c2 > 0) {
+			for (auto x = money_g.begin(); x != money_g.end(); x++)
+			{
+				if (c2 > 0) { *x += c1; c2--; }
+				else if (*x >= m) { *x = 0; answer++; }
+				else goto loop1;
+			}
+		}
+
+		loop1:;
+	}
+
+	int target = 0;
+
+	while (money_g.size())
+	{
+		target += money_g.back(); money_g.pop_back();
+		if (target >= m) { target = 0; answer++;  continue; }
+		if (money_g.size() == 0) break;
+		target += money_g.front(); money_g.pop_front();
+		if (target >= m) { target = 0; answer++;  continue; }
+	}
+
+	cout << answer << "\n";
+	return 0;
 }
